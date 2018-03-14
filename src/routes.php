1aed060fdbc0abb2b86bfc7cf6b04510d154c0ff
HidekiKinjo <kumasan3san@yahoo.co.jp>
2018-03-14 12:15:00 +0900

    started making bss page

--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,18 @@ use Slim\Http\Response;
 
 // Routes
 
+$app->get('/bbs', function (Request $request, Response $response) {
+    return $this->renderer->render($response, 'bbs.phtml');
+});
+
+
+
+
+
+
+
+
+
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
@@ -12,5 +24,4 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
-
 
